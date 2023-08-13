8a00c7e4f0e27b0b7138ab0995969433e47e8cdd
pat <pat@ubuntu-dev.inside.gyokuro.dev>
2023-08-13 09:07:05 -0400

    Testing 1

---
+++ toprod.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+msg=''
+tag=''
+
+main() {
+
+    while getopts 'm:t:' opt; do
+        case "${opt}" in
+            m) msg=${OPTARG} ;;
+            t) tag=${OPTARG} ;;
+            *) usage ;;
+        esac
+    done
+    
+    if [[ $# == 0 ]]; then
+        usage
+    fi
+
+    git add .
+
+    if [[ ${msg} != '' ]]; then
+        git commit -a -m ${msg}
+        git push
+    fi
+
+    if [[ ${tag} != '' ]]; then
+        git tag ${tag}
+        git push origin ${tag}
+    fi
+    exit 0
+}   
+
+usage() {
+    echo ""
+    echo "usage: $0 [ -m | -t ] [ \"Commit message.\" | \"Tag\" ]" 1>&2
+    echo ""
+    exit 1
+}
+
+main "$@"
