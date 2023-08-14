3e96921bc8b29fffdf2f6f91f8c33fb7beaea1bc
Patrick Coleman <pcolemanofficial@gmail.com>
2023-08-14 11:17:28 +0000

    Core functionality complete. TODO: Encryption

--- btool.go
+++ btool.go
@@ -5,14 +5,15 @@ import (
     "flag"
     "os"
     "errors"
+    "strconv"
 )
 
-var MAX_SIZE int = 1024
 
 var pbool_help   = flag.Bool("h", false, "Display usage information.")
 var pstr_bytes   = flag.String("b", "", "A string of bytes: \x41, 0x41, 65")
 var pstr_inFile  = flag.String("i", "", "Read from the provided file.")
 var pstr_outFile = flag.String("o", "", "Write to the provided file.")
+var pint_col     = flag.Int("c", 16, "Output columns. Default: 16.")
 var pstr_enc     = flag.String("e", "", "Encrypt: xor")
 var pstr_dec     = flag.String("d", "", "Decrypt: xor.")
 
@@ -21,25 +22,50 @@ func main() {
     args := os.Args
     flag.Parse()
     if *pbool_help || len(args) < 2 {
-        usage()
+        flag.Usage()
         os.Exit(0)
     }
 
     err := checkArgs(args)
-    checkError(err)
-    
-    var ar_b = make([]byte, MAX_SIZE)
-    if *pstr_bytes != "" {
-        ar_b := str2Bytes(*pstr_bytes)
-    } else if *pstr_inFile != "" {
-        ar_b := fromFile(*pstr_inFile)
-    }
+    checkErr(err)
 
+    ar_b := make([]byte, 0)
+    if *pstr_bytes != "" {
+        *pstr_bytes, err = strconv.Unquote(`"` + *pstr_bytes + `"`)
+        checkErr(err)
+        ar_b = []byte(*pstr_bytes)
+        fmt.Printf("[+] Read %d bytes from stdin.\n", len(ar_b))
+
+    } else if *pstr_inFile != "" {
+        ar_b, err = os.ReadFile(*pstr_inFile)
+        checkErr(err)
+        fmt.Printf("[+] Read %d bytes from %s.\n", len(ar_b), *pstr_inFile)
+    }
 
     if *pstr_enc != "" {
        //TODO ENCRYPT
     } else if *pstr_dec != "" {
        //TODO DECRYPT
+    }
+
+    for i := 0; i < len(ar_b); i++ {
+        if i%(*pint_col) == 0 {
+            fmt.Println()
+        }
+        fmt.Printf("\\x%x", ar_b[i])
+    }
+    fmt.Printf("\n\n")
+
+    if *pstr_outFile != "" {
+        err := os.WriteFile(*pstr_outFile, ar_b, 0600)
+        checkErr(err)
+        fmt.Printf("[+] Wrote %d bytes to %s.\n", len(ar_b), *pstr_outFile)
+    }
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
     }
 }
 
@@ -47,57 +73,10 @@ func checkArgs(args []string) error{
 
     if (*pstr_inFile != "" && *pstr_bytes != "") ||
        (*pstr_enc != "" && *pstr_dec != "") {
-        return errors.New("checkArgs: Invalid flag grouping. "+
+        return errors.New("[-] checkArgs: Invalid flag grouping. "+
                           "Cannot use -b & -i or -e & -d togeather.")
     }
     return nil
-}
-
-func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
-
-func usage() {
-    fmt.Println("This is help.")
-}
-
-func str2Bytes(str_s string) []byte {
-    ar_b := []byte(str_s)
-    return ar_b
-}
-
-func byte2Str(ar_b []byte) string {
-    str_b := string(ar_b)
-    return str_b
-}
-
-func toStdin() {
-
-}
-
-func toFile(ar_b []byte) error {
-    
-}
-
-func fromStdin() {
-
-}
-
-func fromFile(fName string) byte, error {
-
-    f, err := os.Open(fName)
-    defer f.Close()
-    checkError(err)
-    
-    b:= make([]byte, MAX_SIZE)
-    n, err := f.Read(b)
-    checkError(err)
-
-    fmt.Printf("[+] Read %d bytes from %s.\n", n, fName)
-
-    return b, err
 }
 
 // crypto
