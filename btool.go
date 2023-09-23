69110f376cc569b7ed1310777bd9328626d20f56
Patrick Coleman <pcolemanofficial@gmail.com>
2023-09-24 00:53:59 +0530

    minor refactoring.

--- btool.go
+++ btool.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "os"
-    "errors"
-    "strconv"
     "crypto/aes"
     "crypto/cipher"
     "crypto/rand"
+    "errors"
+    "fmt"
+    "flag"
+    "os"
+    "strconv"
 )
 
+var (
+    pbool_help   = flag.Bool("h", false, "Display usage.")
+    pbool_quiet  = flag.Bool("q", false, "Only print final bytes on one line.")
+    pstr_bytes   = flag.String("b", "", "A string of bytes: \"\\x41\\x42...\"")
+    pstr_inFile  = flag.String("i", "", "Read bytes from file.")
+    pstr_outFile = flag.String("o", "", "Output to file.")
+    pint_col     = flag.Int("c", 16, "Output columns.")
+    pstr_enc     = flag.String("e", "", "Encrypt: xor, aes.")
+    pstr_dec     = flag.String("d", "", "Decrypt: xor, aes.")
 
-var pbool_help   = flag.Bool("h", false, "Display usage.")
-var pbool_quiet  = flag.Bool("q", false, "Only print final bytes on one line.")
-var pstr_bytes   = flag.String("b", "", "A string of bytes: \"\\x41\\x42...\"")
-var pstr_inFile  = flag.String("i", "", "Read bytes from file.")
-var pstr_outFile = flag.String("o", "", "Output to file.")
-var pint_col     = flag.Int("c", 16, "Output columns.")
-var pstr_enc     = flag.String("e", "", "Encrypt: xor, aes.")
-var pstr_dec     = flag.String("d", "", "Decrypt: xor, aes.")
+    ErrInvalidEnc   = errors.New("invalid encryption")
+    ErrInvalidDec   = errors.New("invalid decryption")
+    ErrInvalidArgsGroup  = errors.New("cannot use -b & -i or -e & -d togeather")
+)
 
 func main() {
 
@@ -55,12 +60,10 @@ func main() {
     case "aes":
         ar_b, err = e_aes(&ar_b)
         checkErr(err)
-        break
     case "xor":
         e_xor(&ar_b)
-        break
     default:
-        panic("Invalid encryption type.")
+        panic(ErrInvalidEnc)
     }
 
     switch *pstr_dec {
@@ -69,16 +72,14 @@ func main() {
     case "aes":
         ar_b, err = d_aes(&ar_b)
         checkErr(err)
-        break
     case "xor":
         d_xor(&ar_b)
-        break
     default:
-        panic("Invalid decryption type.")
+        panic(ErrInvalidDec)
     }
 
     if *pstr_dec != "" || *pstr_enc != "" {
-        fmt.Print(printBytes(ar_b))
+        fmt.Println(printBytes(ar_b))
     }
 
     if *pstr_outFile != "" {
@@ -111,8 +112,7 @@ func checkArgs(args []string) error{
 
     if (*pstr_inFile != "" && *pstr_bytes != "") ||
        (*pstr_enc != "" && *pstr_dec != "") {
-        return errors.New("[-] checkArgs: Invalid flag grouping. "+
-                          "Cannot use -b & -i or -e & -d togeather.")
+        return ErrInvalidArgsGroup
     }
     return nil
 }
@@ -183,7 +183,7 @@ func e_aes(par_b *[]byte) ([]byte, error) {
     ar_b_ct := gcm.Seal(nonce, nonce, *par_b, nil)
 
     qPrint(fmt.Sprintf("[+] Encrypted %d bytes with AES-256. Key is saved to "+
-    "\"a.key\". \n[+] Result is %d bytes.", len(*par_b), len(ar_b_ct)))
+    "\"a.key\". \n[+] Result is %d bytes.\n", len(*par_b), len(ar_b_ct)))
 
     return ar_b_ct, nil
 }
@@ -212,7 +212,7 @@ func d_aes(par_b_ctnonce *[]byte) ([]byte, error) {
         return nil, err
     }
     qPrint(fmt.Sprintf("[+] Decrypted %d bytes with AES-256. Key used was "+
-    "\"a.key\". \n[+] Result is %d bytes.", len(*par_b_ctnonce), len(ar_b_pt)))
+    "\"a.key\". \n[+] Result is %d bytes.\n", len(*par_b_ctnonce), len(ar_b_pt)))
     return ar_b_pt, nil
 }
 
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/patbcole117/btool
-
-go 1.21.0
